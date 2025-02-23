74f1a73a8326651191d00a4f17c6562b4c1ad59e
nusashi <nusashi@gmail.com>
2025-02-24 06:03:40 +0900

    アイコン調整

--- src/system/icon.py
+++ src/system/icon.py
@@ -7,7 +7,7 @@ def convert_webp_to_ico(webp_path, ico_path):
         # WebPファイルを読み込む
         with Image.open(webp_path) as img:
             # ICOファイルに保存するサイズを指定（例: 16x16, 32x32, 64x64）
-            sizes = [(16, 16), (32, 32), (48, 48), (64, 64), (128, 128)]
+            sizes = [(16, 16), (32, 32), (48, 48), (64, 64), (128, 128), (256, 256)]
             # ICOファイルに保存
             img.save(ico_path, format="ICO", sizes=sizes)
         print(f"Successfully converted {webp_path} to {ico_path}")
